f30032bacd1e81b7a3eb4c44a6edc808b15bc8bc
C1291392 Marcos Humberto Alves Felipe <c1291392@interno.bb.com.br>
2024-08-03 14:40:06 -0300

    implementacao desafio

---
+++ conta-banco/src/Conta.java
@@ -0,0 +1,27 @@
+public class Conta {
+   private int numero;
+   private String agencia;
+   private String nomeCliente;
+   private Double saldo;
+
+
+   public void setNumero(int numero) {
+    this.numero = numero;
+   }
+
+   public void setAgencia(String agencia) {
+    this.agencia = agencia;
+   }
+
+   public void setNomeCliente(String nomeCliente) {
+    this.nomeCliente = nomeCliente;
+   }
+
+   public void setSaldo(Double saldo) {
+    this.saldo = saldo;
+   }
+
+   
+
+   
+}
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner entrada = new Scanner(System.in);
+        Conta conta = new Conta();
+
+        System.out.println("Digite a agencia da conta a ser criada:");
+        conta.setAgencia(entrada.nextLine());
+
+        System.out.println("Digite o número da conta a ser criada:");
+        conta.setNumero(entrada.nextInt());
+
+        entrada.nextLine();
+
+        System.out.println("Digite o nome do cliente da conta a ser criada:");
+        conta.setNomeCliente(entrada.nextLine());
+
+        System.out.println("Digite o saldo inicial da conta a ser criada:");
+        conta.setSaldo(entrada.nextDouble());
+
+        System.out.println("Conta criada: ");
+
+        // Fechar o Scanner
+        entrada.close();
+
+        
     }
 }
